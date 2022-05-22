96f3adbccc275369cc3751e839d63af93237acb5
rkirk430 <r.kirk430@gmail.com>
2022-05-22 13:57:22 -0400

    Prior to Netlify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,31 @@
-function Home(props) {
-    return <h1>Home</h1>;
-  }
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap"
+
+const Home = (props) => {
+    return(
+      <div>
+        <>
+        <Container> 
+          <Row className="homepage-header">
+            <Col sm={8}></Col>
+              <h4> Alternative Market Research for the Disruptive Economy. </h4>
+            <Col sm={4}></Col>
+              <h6> Quantstamp analyzes millions of datapoints to provide granular, actionable insights to underlying market sentiment </h6>
+          </Row>
+        </Container>
+        <Container>
+          <Row className="homepage-benefits-container">
+            <Col sm={4}>API Model Integration
+              <h6> but what does it mean? </h6>
+            </Col>
+            <Col sm={4}>Real Time Trade Alerts</Col>
+            <Col sm={4}>Improving Risk-Adjusted Returns</Col>
+          </Row>
+        </Container>
+        </>
+      </div>
+      
+    ) 
+  };
   
   export default Home;
