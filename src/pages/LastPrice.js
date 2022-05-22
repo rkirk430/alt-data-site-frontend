0ea928bdb75c6bad05b4fb3e86998d89bfa5c1e5
rkirk430 <r.kirk430@gmail.com>
2022-05-22 15:31:10 -0400

    API Frontend data rendering

--- src/pages/LastPrice.js
+++ src/pages/LastPrice.js
@@ -1,4 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+
 function LastPrice(props) {
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const params = useParams();
+  // console.log(params);
+  const symbol = params.symbol;
+  // console.log(symbol);
+
+  const url =`https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=bd2e518d854f51531fbc091749c45bdf`;
+  
+
+  const [price, setPrice] = useState("null");
+
+  const getPrice = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data[0].symbol);
+    setPrice(data);
+  };
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return <h1>Last Price Page</h1>;
 }
 
