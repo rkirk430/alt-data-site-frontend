d9df37a245cb3db045b05633932cdbf25fcd7f3f
rkirk430 <r.kirk430@gmail.com>
2022-05-21 22:03:39 -0400

    Router Setup

---
+++ src/pages/Government.js
@@ -0,0 +1,5 @@
+function Government(props) {
+    return <h1>Gov't Trades Page</h1>;
+  }
+  
+  export default Government;
---
+++ src/pages/Sentiment.js
@@ -0,0 +1,5 @@
+function Sentiment(props) {
+    return <h1>Sentiment Index</h1>;
+  }
+  
+  export default Sentiment;
